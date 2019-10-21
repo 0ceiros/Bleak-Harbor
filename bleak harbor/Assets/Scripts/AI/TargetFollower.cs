d280cb57d707c1c9b72d28799eea04e4171e3173
BearPro <bearpro@outlook.com>
2019-10-21 22:49:20 +0300

    Добавлен базовый класс для сущностей и несколько простых реализаций для примера примитивного искусственного интеллекта.

---
+++ bleak harbor/Assets/Scripts/AI/TargetFollower.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BleakHarbor.Assets.Scripts.AI
+{
+    class TargetFollower : Core.EntityBase
+    {
+        /// <summary>
+        /// Цель, за которой нужно следить.
+        /// </summary>
+        public Transform target;
+        public override Func<Vector2> DirectionFactory => GetDirection;
+
+        private Vector2 GetDirection()
+        {
+            var direction = new Vector2(target.position.x, target.position.y) - this.Position;
+            direction = Vector2.ClampMagnitude(direction, 1);
+            return direction;
+        }
+    }
+}
