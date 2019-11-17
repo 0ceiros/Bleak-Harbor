1bae56a914a3391a5bfea387953e9bbb1a58a296
BearPro <bearpro@outlook.com>
2019-11-17 20:41:52 +0300

    Добавлены путевые точки. Добавлена сущность, способная передвигаться по путевым точкам. Добавлен префаб путевой точки.

---
+++ bleak harbor/Assets/Scripts/AI/Waypoint.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static BleakHarbor.Core.Utils;
+
+namespace BleakHarbor.AI
+{
+    /// <summary>
+    /// Представляет точку патрулирования.
+    /// Из таких точек собирается маршрут патрулирования.
+    /// </summary>
+    class Waypoint : MonoBehaviour
+    {
+        private new Transform transform;
+
+        /// <summary>
+        /// Показывает следующую точку патрулирования.
+        /// </summary>
+        public Waypoint Next { get => next; }
+        [SerializeField]
+        private Waypoint next;
+        
+
+        /// <summary>
+        /// Показывает, что на этой точке следует остановиться.
+        /// </summary>
+        public bool StopHere { get; }
+        [SerializeField]
+        private bool stopHere;
+
+        
+
+        public Vector2 Position { get => new Vector2(transform.position.x, transform.position.y); }
+
+        public void Start()
+        {
+            transform = this.GetComponent<Transform>();
+        }
+
+        public void Update()
+        {
+            Debug.DrawLine(Position, Next.Position);
+        }
+    }
+}
---
+++ bleak harbor/Assets/Scripts/AI/WaypointWalker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace BleakHarbor.AI
+{
+    public class WaypointWalker : Core.EntityWithFraction
+    {
+        [SerializeField]
+        private Waypoint start;
+
+        private WaypointDirectionFactory directionFactory;
+
+        public override Func<Vector2> DirectionFactory => directionFactory.GetDirection;
+
+        public new void Start()
+        {
+            base.Start();
+            directionFactory = new WaypointDirectionFactory(start, () => Position);
+        }
+    }
+
+}
